29130f2ceceb052a82cec02c6f92ae7391f6431c
Louis-Philippe Lemieux Perreault <louis-philippe.lemieux.perreault@statgen.org>
2016-01-29 14:44:02 -0500

    Refactoring and better phenotype/sample extraction

--- run_metaskat.py
+++ run_metaskat.py
@@ -4,13 +4,17 @@ from __future__ import print_function
 
 import os
 import sys
+import shutil
 import logging
 import argparse
+import subprocess
+from tempfile import mkdtemp
 
 import yaml
 import numpy as np
 import pandas as pd
 
+from rpy2 import rinterface
 import rpy2.robjects as robjects
 from rpy2.robjects import Formula as rformula
 import rpy2.robjects.numpy2ri as numpy2ri
@@ -19,6 +23,9 @@ from rpy2.robjects.packages import importr as rimport
 
 # Activating numpy array to rpy2 objects automatic conversion
 numpy2ri.activate()
+
+# Removing R output
+rinterface.set_writeconsole(None)
 
 # Loading required R libraries
 metaskat = rimport("MetaSKAT")
@@ -44,69 +51,50 @@ def main():
     args = parse_args()
     check_args(args)
 
+    # Creating the output directory, if required
+    if not os.path.isdir(args.output):
+        os.mkdir(args.output)
+
+    # Creating a temporary directory
+    tmp_dir = mkdtemp(prefix="metaskat_tmp_", dir=args.output)
+
     # Reading the YAML configuration
     cohort_information = read_cohort_configuration(args.yaml_file)
 
-    # Creating the null model
-    generate_meta_files(cohort_information, args.gene_list,
-                        out_dir=args.output)
+    # Running SKAT on individual cohorts
+    execute_skat(cohort_information, args.gene_list, o_prefix=args.output,
+                 tmp_dir=tmp_dir)
+
+    # Deleting temporary directory
+    if os.path.isdir(tmp_dir):
+        shutil.rmtree(tmp_dir)
 
 
-def generate_meta_files(cohorts, genes, out_dir):
-    """Generating the SKAT meta files."""
-    logger.info("Generating the meta files")
+def execute_skat(cohorts, genes, o_prefix, tmp_dir):
+    """Generating the SKAT meta files.
 
-    list_mssd = []
-    list_minfo = []
-    meta_cohort_info = None
+    Args:
+        cohorts (dict): information about individual cohorts.
+        genes (str): name of the file containing the SNP set (by gene).
+        o_prefix (str): the output prefix.
+        tmp_dir (str): the name of the temporary directory.
+
+    """
     for cohort, cohort_info in cohorts.items():
-        logger.info("  - " + cohort)
+        # Getting the plink file prefix (str) and the phenotype (DataFrame)
+        plink_prefix, pheno = get_analysis_data(
+            plink_prefix=cohort_info["prefix"],
+            pheno=cohort_info["phenotype"],
+            covariates=cohort_info["covariates"],
+            pheno_fn=cohort_info["phenotype_file"],
+            fid=cohort_info.get("family_id", "FID"),
+            iid=cohort_info.get("individual_id", "IID"),
+            tmp_dir=os.path.join(tmp_dir, cohort),
+        )
 
-        # Creating the required variables
-        prefix = cohort_info["prefix"]
-        pheno_name = cohort_info["phenotype"]
-        pheno_filename = cohort_info["phenotype_file"]
+        # Getting the required information
         covariates = cohort_info["covariates"]
-
-        # Plink binary files
-        bed = prefix + ".bed"
-        bim = prefix + ".bim"
-        fam = prefix + ".fam"
-
-        # Reading the FAM file
-        fam = pd.read_csv(fam, sep=" ",
-                          names=["fid", "iid", "father", "mother", "gender",
-                                 "status"])
-        fam = fam.set_index("iid", verify_integrity=True)
-
-        # Reading the phenotype and covariates
-        pheno = pd.read_csv(pheno_filename, sep="\t")
-        pheno = pheno.set_index("IID", drop=False, verify_integrity=True)
-
-        # Keeping only samples with genotypes
-        pheno = pheno.loc[fam.index, ]
-
-        # Checking we have all required information
-        if (len(pheno) != len(fam)) or (not all(pheno.index == fam.index)):
-            logger.critical("{}: missing sample information".format(cohort))
-            sys.exit(1)
-
-        # Checking the covariates
-        for covariate in covariates:
-            if covariate not in pheno.columns:
-                logger.critical("{}: missing covariate '{}'".format(
-                    pheno_filename,
-                    covariate,
-                ))
-                sys.exit(1)
-
-        # Checking the phenotype
-        if pheno_name not in pheno.columns:
-            logger.critical("{}: missing phenotype '{}'".format(
-                pheno_filename,
-                pheno_name,
-            ))
-            sys.exit(1)
+        pheno_name = cohort_info["phenotype"]
 
         # Creating the formula
         formula = rformula("{} ~ {}".format(
@@ -124,18 +112,139 @@ def generate_meta_files(cohorts, genes, out_dir):
         model = skat.SKAT_Null_Model(formula, type="C")
 
         # The output files
-        mssd = os.path.join(out_dir, cohort + ".MSSD")
-        minfo = os.path.join(out_dir, cohort + ".MInfo")
+        mssd = os.path.join(o_prefix, cohort + ".MSSD")
+        minfo = os.path.join(o_prefix, cohort + ".MInfo")
 
         # Generating the meta files
-        metaskat.Generate_Meta_Files(model, bed, bim, genes, mssd, minfo,
-                                     fam.shape[0],
-                                     File_Permu=robjects.r("NULL"))
+        try:
+            metaskat.Generate_Meta_Files(model, plink_prefix + ".bed",
+                                         plink_prefix + ".bim", genes, mssd,
+                                         minfo, pheno.shape[0],
+                                         File_Permu=robjects.r("NULL"))
+        except Exception as e:
+            logger.critical("problem with SKAT\n{}".format(e))
+            sys.exit(1)
 
-        # Creating lists with names of mssd and minfo files
-        list_mssd.append(mssd)
-        list_minfo.append(minfo)
 
+def get_analysis_data(plink_prefix, pheno, covariates, fid, iid, pheno_fn,
+                      tmp_dir):
+    """Extract phenotypes and genomic information about the sample.
+
+    Args:
+        plink_prefix (str): the prefix of the Plink files.
+        pheno (str): the name of the phenotype (in the phenotype file).
+        covariates (list): the list of covariate names (in the pheno file).
+        fid (str): the name of the family ID column (in the pheno file).
+        iid (str): the name of the individual ID column (in the pheno file).
+        pheno_fn (str): the name of the phenotype file.
+        tmp_dir (str): the name of the temporary file.
+
+    Returns:
+        tuple: first element is the Plink prefix for the genotype data, the
+               second element is the DataFrame containing the phenotype and
+               covariate data.
+
+    """
+    # Reading the FAM file
+    fam = pd.read_csv(plink_prefix + ".fam", sep=" ",
+                      names=["fid", "iid", "father", "mother", "gender",
+                             "status"])
+    fam = fam.set_index(["fid", "iid"], verify_integrity=True)
+
+    # Reading the phenotype and covariates
+    phenotypes = pd.read_csv(pheno_fn, sep="\t")
+
+    # Checking that we have all the required column in the phenotype file
+    req_col = [fid, iid, pheno] + covariates
+    for col in req_col:
+        if col not in phenotypes.columns:
+            logger.critical("{}: missing column '{}'".format(pheno_fn, col))
+            sys.exit(1)
+
+    # Keeping only the required column (and setting the index)
+    phenotypes = phenotypes[req_col].set_index(
+        [fid, iid],
+        verify_integrity=True,
+    ).dropna().sortlevel()
+
+    # Finding the intersection between phenotype and sample file
+    same_samples = fam.index.intersection(phenotypes.index)
+    nb_same = len(same_samples.tolist())
+
+    if nb_same == 0:
+        logger.critical("{} vs {}: no sample in common".format(
+            plink_prefix + ".fam",
+            pheno_fn,
+        ))
+        sys.exit(1)
+
+    if (nb_same == fam.shape[0]) and (nb_same == phenotypes.shape[0]):
+        # We have the same samples, hence, we only return the ordered phenotype
+        # data (same order as FAM file)
+        phenotypes = phenotypes.loc[fam.index, ]
+        if fam.index.tolist() != phenotypes.index.tolist():
+            logger.critical("something really bad happened...")
+            sys.exit(1)
+        return plink_prefix, phenotypes
+
+    # We need extraction, so we create the directory, if required
+    if not os.path.isdir(tmp_dir):
+        os.mkdir(tmp_dir)
+
+    # Extracting required samples using plink
+    new_prefix = extract_plink(plink_prefix, os.path.join(tmp_dir, "subset"),
+                               same_samples.tolist())
+
+    # Rerunning the function
+    return get_analysis_data(new_prefix, pheno, covariates, fid, iid, pheno_fn,
+                             tmp_dir)
+
+
+def extract_plink(i_prefix, o_prefix, samples):
+    """Extract a list of samples from a Plink binary file.
+
+    Args:
+        i_prefix (str): the prefix of the input Plink file.
+        o_prefix (str): the prefix of the output Plink file.
+        samples (list): the list of samples to extract
+
+    """
+    # Creating the list of samples to extract
+    with open(o_prefix + ".to_extract", "w") as f:
+        for fid, iid in samples:
+            print(fid, iid, file=f)
+
+    # Executing Plink (for extraction)
+    plink_command = [
+        "plink",
+        "--noweb",
+        "--bfile", i_prefix,
+        "--keep", o_prefix + ".to_extract",
+        "--make-bed",
+        "--out", o_prefix,
+    ]
+    try:
+        proc = subprocess.Popen(plink_command, stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE)
+    except OSError:
+        logger.critical("plink: missing binary")
+        sys.exit(0)
+
+    try:
+        stdout, stderr = proc.communicate()
+
+    except:
+        logger.critical("problem with plink extraction")
+        sys.exit(0)
+
+    if proc.returncode != 0:
+        logger.critical("there was a problem with plink\n" + stderr.decode())
+        sys.exit(1)
+
+    return o_prefix
+
+
+def execute_meta_analysis(cohorts, genes, out_dir):
     # Combining the results
     mssd_files = np.array(list_mssd)
     minfo_files = np.array(list_minfo)
@@ -171,14 +280,12 @@ def generate_meta_files(cohorts, genes, out_dir):
                         quote=False, sep="\t", row_names=False, col_names=True)
 
     # Printing final results per cohort
-    i = 0
-    for cohort, cohort_info in cohorts.items():
-        name = cohort + "_info"
-        name = meta_cohort_info[meta_cohort_info.names.index("EachInfo")][i]
-        name = name[name.names.index("Info")]
-        name.to_csvfile(os.path.join(out_dir, cohort + ".MetaInfo.txt"),
-                        sep="\t", quote=False, row_names=False)
-        i = i + 1
+    each_cohort_info = meta_cohort_info[meta_cohort_info.names.index("EachInfo")]
+    for i, cohort in enumerate(cohort_names):
+        final_info = each_cohort_info[i]
+        final_info = final_info[final_info.names.index("Info")]
+        final_info.to_csvfile(os.path.join(out_dir, cohort + ".MetaInfo.txt"),
+                              sep="\t", quote=False, row_names=False)
 
 
 def read_cohort_configuration(fn):
@@ -245,9 +352,6 @@ def check_args(args):
     if not os.path.isfile(args.gene_list):
         logger.critical("{}: no such file".format(args.gene_list))
         sys.exit(1)
-
-    if not os.path.isdir(args.output):
-        os.mkdir(args.output)
 
 
 def parse_args():
